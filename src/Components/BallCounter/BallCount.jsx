7237b72f170289a713df8c4f796ba21e202fec1a
HasibulHRony <hasibulhrony7@gmail.com>
2025-09-25 09:08:18 +0600

    no Ball functionality added

--- src/Components/BallCounter/BallCount.jsx
+++ src/Components/BallCounter/BallCount.jsx
@@ -11,10 +11,17 @@ const BallCout = () => {
             setBall(<p>Over Completed</p>)
         }
     }
+
+    const handleNoBall = () =>{
+        const ballSum = ball - 1
+        setBall(ballSum);
+        alert("Ops! it was a no ball! free hit! you should do an extra ball!")
+    }
     return (
         <div>
             <div style={{ textAlign: "text-center", padding: "10px", border: "2px solid yellow", borderRadius: "10px", margin: "10px", fontSize: "30px" }}>Ball: {ball}</div>
             <button onClick={handleBall}>1 Ball</button>
+            <button onClick={handleNoBall}>No Ball</button>
         </div>
     );
 };
