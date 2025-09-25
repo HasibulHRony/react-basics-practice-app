3c6b54d3045537de9a244c056a08238d1c970e17
HasibulHRony <hasibulhrony7@gmail.com>
2025-09-25 11:38:26 +0600

    more counter functionality added.

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -2,16 +2,30 @@ import React, { useState } from 'react';
 
 const Counter = () => {
     const [count, setCount] = useState(0)
-    const handleCount = () => {
+    const handleCountIncrease = () => {
             const plusCount = count + 1;
             setCount(plusCount)   
     }
+
+    const handleCountDecrease = () =>{
+        const minsCount = count-1;
+        setCount(minsCount)
+    }
+
+    const handleReset = () =>{
+       const reset = 0;
+        setCount(reset)
+    }
+
     return (
         <div>
             <div style={{ textAlign: "text-center", padding: "10px", border: "2px solid yellow", borderRadius: "10px", margin: "10px", fontSize: "30px" }}>
                 Number: {count}
             </div>
-            <button style={{ fontSize: "30px" }} onClick={handleCount}>++</button>
+            <button style={{ fontSize: "30px" }} onClick={handleCountIncrease}>++</button>
+            <button style={{ fontSize: "30px" }} onClick={handleCountDecrease}>--</button>
+            <button style={{ fontSize: "30px" }} onClick={handleReset}>Reset</button>
+            
         </div>
     );
 };
--- src/Components/Friends/Friends.jsx
+++ src/Components/Friends/Friends.jsx
@@ -2,7 +2,7 @@ import  { use } from 'react';
 
 const Friends = ({friendsPromise}) => {
     const friends = use(friendsPromise)
-    console.log(friends)
+    
     return (
         <div>
             <h1>total friends: {friends.users.length}</h1>
